577d39c32290980ff03344a80c65f57d61035383
suntao <suntao@DESKTOP-P8IU6JF>
2019-09-17 09:26:23 +0800

    date

--- antd-st/src/components/Header/header.js
+++ antd-st/src/components/Header/header.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import { Row, Col } from 'antd'
+import './header.css'
+import Util from '../../uitls/uitls';
 
 class Header extends React.Component{
-
+    state = {}
+    componentWillMount(){
+        this.setState({
+            userName:'浅吟风迹'
+        })
+        setInterval(()=>{
+            let sysTime = Util.formateDate(new Date().getTime());
+            this.setState({sysTime});
+        })
+    }
     render(){
         return (
-            <div>
-                header
+            <div className="header">
+                <Row className="header-top">
+                    <Col span = {24}>
+                        <span>欢迎，{this.state.userName}</span>
+                        <a href="#">退出</a>
+                    </Col>
+                </Row>
+                <Row className="breadcrumb">
+                    <Col span = {4} className="breadcrumb-tit">
+                       首页
+                    </Col>
+                    <Col span = {20} className="weather">
+                        <span className="weather-date">{this.state.sysTime}</span>
+                        <span className="weather-con">晴转多云</span>
+                    </Col>
+                </Row>
             </div>
         )
     }
