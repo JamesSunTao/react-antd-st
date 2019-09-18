9ec95890d4a0d1ac0a7f5333864ed64ec66ad78e
suntao <suntao@DESKTOP-P8IU6JF>
2019-09-18 17:36:41 +0800

    add redux

--- antd-st/src/index.js
+++ antd-st/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './router';
 import * as serviceWorker from './serviceWorker';
-import Home from './pages/router_demo/router3/router'
-
-ReactDOM.render(<Router/>, document.getElementById('root'));
+ReactDOM.render(
+        <Router/>
+    , 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ antd-st/src/redux/reducer/index.js
@@ -0,0 +1,15 @@
+import { type } from '../action';
+
+const ebikeData = (state, action) => {
+    switch (action.type) {
+        case type.SWITCH_MENU:
+            return {
+                ...state,
+                menuName:action.menuName
+            };
+        default:
+            return {...state};
+    }
+};
+
+export default ebikeData;
